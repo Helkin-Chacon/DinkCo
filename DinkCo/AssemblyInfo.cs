1b6351d8002f26d9ae9de14203b8f70c9c61850d
Helkin-Chacon <63528430+Helkin-Chacon@users.noreply.github.com>
2021-02-09 17:07:19 -0500

    Interface Grafica Inicial

---
+++ DinkCo/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Quicksand_Bold.otf")]
+
+
+[assembly: ExportFont("Quicksand_Book.otf")]
+[assembly: ExportFont("Quicksand_Light.otf")]
+[assembly: ExportFont("fontdink.ttf", Alias = "FontDink")]
---
+++ DinkCo/Views/MenusPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Syncfusion.SfNavigationDrawer.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DinkCo.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenusPage : ContentPage
+	{
+		public MenusPage()
+        {
+            InitializeComponent();
+            SfNavigationDrawer hola = navigationDrawer1;
+         
+            bt.Clicked += (object sender, System.EventArgs e) =>
+            {
+                if (simTab.SelectedIndex == 4)
+                {
+                    hola.ToggleDrawer();
+                    simTab.SelectedIndex = 0;
+                }
+                else
+                {
+                    simTab.SelectedIndex = 4;
+                    hola.ToggleDrawer();
+                }
+            };
+
+            Button GoToGeneralButton = new Button();
+            GoToGeneralButton.Clicked += OnTapGestureRecognizerTapped1;
+        }
+        void OnTapGestureRecognizerTapped(object sender, EventArgs args)
+        {
+            if (simTab.SelectedIndex == 4)
+            {
+                SfNavigationDrawer hola = navigationDrawer1;
+                hola.ToggleDrawer();
+                simTab.SelectedIndex = 0;
+            }
+            else
+            {
+                simTab.SelectedIndex = 0;
+            }
+        }
+        void OnTapGestureRecognizerTapped1(object sender, EventArgs args)
+        {
+            if (simTab.SelectedIndex == 4)
+            {
+                SfNavigationDrawer hola = navigationDrawer1;
+                hola.ToggleDrawer();
+                simTab.SelectedIndex = 1;
+            }
+            else
+            {
+                simTab.SelectedIndex = 1;
+            }
+        }
+
+        void OnTapGestureRecognizerTapped2(object sender, EventArgs args)
+        {
+            if (simTab.SelectedIndex == 4)
+            {
+                SfNavigationDrawer hola = navigationDrawer1;
+                hola.ToggleDrawer();
+                simTab.SelectedIndex = 2;
+            }
+            else
+            {
+                simTab.SelectedIndex = 2;
+            }
+        }
+        void OnTapGestureRecognizerTapped3(object sender, System.EventArgs e)
+        {
+            if (simTab.SelectedIndex == 4)
+            {
+                SfNavigationDrawer hola = navigationDrawer1;
+                hola.ToggleDrawer();
+                simTab.SelectedIndex = 3;
+            }
+            else
+            {
+                simTab.SelectedIndex = 3;
+            }
+        }
+        private void GoToGeneral(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
